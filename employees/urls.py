4f7e814d31bf0b53946dbc9da6769ecb70c04eda
yuhi.kondo <yuhi.kondo@hit-inc.jp>
2025-04-23 13:45:20 +0900

    ・EmployeeUserFormの設定 ・不要なviewであるcreateを削除 ・login_requiredとstaff_member_requiredの順序交換

--- employees/urls.py
+++ employees/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.employee_list, name='employee_index'),
     path('new/', views.employee_new, name='employee_new'),
-    path('create/', views.employee_create, name='employee_create'),
+    #path('create/', views.employee_create, name='employee_create'),
     path('<int:pk>/confirm_delete/',views.employee_confirm_delete, name='employee_confirm_delete' ),
     path('<int:pk>/edit/', views.employee_edit, name='employee_edit'),
     path('<int:pk>/update/', views.employee_update, name='employee_update'),
--- employees/views.py
+++ employees/views.py
@@ -2,40 +2,50 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 from .models import Employee
+from .forms import EmployeeUserForm
 
 # Create your views here.
 def home(request):
+    '''トップページ'''
     return render(request, 'home.html')
 
+""" @login_required
 @staff_member_required
-@login_required
 def employee_create(request):
+    '''新規作成画面'''
     if request.method == 'POST':
         name = request.POST['name']
         email = request.POST['email']
         Employee.objects.create(name=name, email=email)
         return redirect('employee_index')
-    return redirect('employee_new')
+    return redirect('employee_new') """
 
-@staff_member_required
 @login_required
+@staff_member_required
 def employee_new(request):
-    return render(request, 'employees/employee_new.html')
+    if request.method == 'POST':
+        form = EmployeeUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('employee_index')
+    else:
+        form = EmployeeUserForm()
+    return render(request, 'employees/employee_form.html', {'form':form})
 
-@staff_member_required
 @login_required
+@staff_member_required
 def employee_list(request):
     employees = Employee.objects.all()
     return render(request, 'employees/employee_list.html', {'employees': employees})
 
-@staff_member_required
 @login_required
+@staff_member_required
 def employee_edit(request, pk):
     employee = get_object_or_404(Employee, pk=pk)
     return render(request, 'employees/employee_edit.html', {'employee':employee})
 
-@staff_member_required
 @login_required
+@staff_member_required
 def employee_update(request, pk):
     employee = get_object_or_404(Employee, pk=pk)
     if request.method == 'POST':
@@ -45,15 +55,15 @@ def employee_update(request, pk):
         return redirect('employee_index')
     return redirect('employee_edit', pk=pk)
 
-@staff_member_required
 @login_required
+@staff_member_required
 def employee_delete(request, pk):
     employee = get_object_or_404(Employee, pk=pk)
     employee.delete()
     return redirect('employee_index')
 
-@staff_member_required
 @login_required
+@staff_member_required
 def employee_confirm_delete(request, pk):
     employee = get_object_or_404(Employee, pk=pk)
     if request.method == 'POST':
